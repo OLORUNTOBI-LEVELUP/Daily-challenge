6c5d9499647ea20a6d2028fa8df7f38307c00020
OLORUNTOBI-LEVELUP <awoderuoloruntobi@gmail.com>
2019-01-19 21:27:32 +0100

    commited changes

--- calculator challenge/calculator.js
+++ calculator challenge/calculator.js
@@ -1,16 +1,18 @@
-function areaOfaParallogram(base, height) {
-    return base * height;
-};
-console.log(areaOfaParallogram(base, height).toFixed(2));
+function areaOfaParallogram(base, height, decimalPlace) {
+    const area = base * height;
+    return area.toFixed(decimalPlace);
+}
+console.log(areaOfaParallogram(base, height, decimalPlace));
 
 function volumeOfaCylinder(radius, height) {
-    return pi * radius * radius * height
-};
-const pi = 3.142
+    const pi = 3.142;
+    const area = pi * radius * radius * height;
+    return area;
+}
 console.log(volumeOfaCylinder(radius, height));
 
 
 function areaOfaRectangle(length, breadth) {
-    return length * breadth
-};
+    return length * breadth;
+}
 console.log(areaOfaRectangle(length, breadth));
