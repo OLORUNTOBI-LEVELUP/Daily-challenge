569296478fade196af3c1cf3b3e5f9bf57eaa409
OLORUNTOBI-LEVELUP <awoderuoloruntobi@gmail.com>
2019-01-19 19:58:39 +0100

    calcuator challenge commit

---
+++ calculator challenge/calculator.js
@@ -0,0 +1,16 @@
+function areaOfaParallogram(base, height) {
+    return base * height;
+};
+console.log(areaOfaParallogram(base, height).toFixed(2));
+
+function volumeOfaCylinder(radius, height) {
+    return pi * radius * radius * height
+};
+const pi = 3.142
+console.log(volumeOfaCylinder(radius, height));
+
+
+function areaOfaRectangle(length, breadth) {
+    return length * breadth
+};
+console.log(areaOfaRectangle(length, breadth));
